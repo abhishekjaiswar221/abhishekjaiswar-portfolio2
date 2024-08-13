257893fdd597c5357a33a5ce5422c8814e8f6515
Abhishek D Jaiswar <devabhishekjaiswar@gmail.com>
2024-08-13 19:44:12 +0530

    Add projects section in the site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 // import HeaderNavigation from "@/components/headerNavigation";
 import Hero from "@/components/hero";
+import Projects from "@/components/projects";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col gap-10 items-center justify-center">
+    <main className="flex min-h-screen flex-col gap-10 items-center justify-center px-20">
       {/* <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
@@ -115,6 +116,9 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
+      </section>
+      <section id="projects">
+        <Projects />
       </section>
     </main>
   );
