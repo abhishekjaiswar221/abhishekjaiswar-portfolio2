7619732a562658f50fc1c065eeea75b7910f56cb
Abhishek D Jaiswar <devabhishekjaiswar@gmail.com>
2024-08-14 19:41:12 +0530

    Add a top loading bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "@/styles/globals.css";
 import HeaderNavigation from "@/components/headerNavigation";
 import FooterNavigation from "@/components/footerNavigation";
 import { cn } from "@/lib/utils";
+import NextTopLoader from "nextjs-toploader";
 
 const inter = Inter({ subsets: ["latin"] });
 export const runtime = "edge";
@@ -22,6 +23,18 @@ export default function RootLayout({
     <html lang="en">
       <body className={cn(inter.className, "h-full")}>
         <div className="h-full flex flex-col items-center">
+          <NextTopLoader
+            color="#282f3c"
+            initialPosition={0.08}
+            crawlSpeed={200}
+            height={2}
+            crawl={true}
+            showSpinner={false}
+            easing="ease"
+            speed={200}
+            shadow="0 0 10px #282f3c,0 0 5px #282f3c"
+            zIndex={1600}
+          />
           <HeaderNavigation />
           {children}
           <FooterNavigation />
